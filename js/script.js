33d9d8c2434a56f74589a7222c42167f8814637b
dmck0y <dmckoy85@gmail.com>
2014-10-28 14:28:52 -0400

    add routing

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
 (function(){
-
-	var container = document.querySelector('div[data-js="packery_obj"]');
-	var packery = new Packery(container, {
-		itemSelector: '.item',
-		gutter: 0
+	//routing click handler
+	$('body').on('click','a.route',function(e) {
+		if (App.router.goto($(this).attr('href'))) {
+			e.preventDefault();
+		}
 	});
-
+	
+	//opens side menu in mobile view
 	$('[data-js="menu"]').on('click', function(e){
 		e.preventDefault();
-		
 		$('.desktop').toggleClass('open');
 	})
+
+	//setups packery.js
+	var container = $('[data-js="packery_obj"]').packery();
+
+	$(container).on('click', '.item', function(e) {
+		var target = $(e.target).parent();
+		var isBig = target.hasClass('big');
+		container.children().removeClass('big');
+
+		target.toggleClass('big');
+
+		if (isBig) {
+			container.packery();
+		} else {
+			container.packery('fit', e.target);
+		}
+	});
+
 }());
